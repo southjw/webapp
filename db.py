f7e43946f1a7fce70ca93ed78651bd461e924b48
southjw <southjw@163.com>
2015-09-07 15:25:09 +0800

    add file

---
+++ db.py
@@ -0,0 +1,23 @@
+##import sqlite3
+##conn = sqlite3.connect('C:/Users/jw/Desktop/500.db')
+##cursor = conn.cursor()
+##cursor.execute('select * from user')
+##values = cursor.fetchall()
+##print (values)
+##cursor.close()
+##conn.close()
+
+import mysql.connector
+conn=mysql.connector.connect(user='@localhost',password='',database='test',use_unicode=True)
+##cursor=conn.cursor()
+##cursor.execute('create table user(id int primary key,name varchar(20))')
+##cursor.execute('insert into user(id,name) values(1,%s)',['jj'])
+##print ('execute effect row number:',cursor.rowcount)
+##conn.commit()
+##cursor.close()
+cursor = conn.cursor()
+cursor.execute('select * from user')
+values = cursor.fetchall()
+print (values)
+cursor.close()
+conn.close()
