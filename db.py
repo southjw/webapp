1b7aa481d659787f25384bfd60821600896ff9b4
southjw <southjw@163.com>
2015-09-07 18:03:24 +0800

    learn...

--- db.py
+++ db.py
@@ -1,23 +1,81 @@
-##import sqlite3
-##conn = sqlite3.connect('C:/Users/jw/Desktop/500.db')
-##cursor = conn.cursor()
-##cursor.execute('select * from user')
-##values = cursor.fetchall()
-##print (values)
-##cursor.close()
-##conn.close()
+import threading
+class _Engine(object):
+    def __init__(self,connect):
+        self._connect=connect
+    def connect(self):
+        return self._connect()
 
-import mysql.connector
-conn=mysql.connector.connect(user='@localhost',password='',database='test',use_unicode=True)
-##cursor=conn.cursor()
-##cursor.execute('create table user(id int primary key,name varchar(20))')
-##cursor.execute('insert into user(id,name) values(1,%s)',['jj'])
-##print ('execute effect row number:',cursor.rowcount)
-##conn.commit()
-##cursor.close()
-cursor = conn.cursor()
-cursor.execute('select * from user')
-values = cursor.fetchall()
-print (values)
-cursor.close()
-conn.close()
+engine = None
+
+class _DbCtx(threading.local):
+    def __init__(self):
+        self.connection = None
+        self.transactions = 0
+    def is_init(self):
+        return not self.connection is None
+
+    def init(self):
+        self.connection = _LasyConnection()
+        self.transactions = 0
+
+    def cleanup(self):
+        self.connection.cleanup()
+        self.connection = None
+
+    def cursor(self):
+        return self.connection.cursor()
+
+_db_ctx = _DbCtx()
+
+
+class _ConnectionCtx(object):
+    def __enter__(self):
+        global _db_ctx
+        self.should_clearup = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_clearup = True
+            return self
+
+    def __exit__(self,exctype,excvalue,traceback):
+        global _db_ctx
+        if (self.should_cleanup):
+            _db_ctx.cleanup()
+
+    def connection():
+        return _ConnectionCtx()
+
+class _TransactionCtx(object):
+    def __enter__(self):
+        global _db_ctx
+        self.should_close_conn = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_close_conn = True
+        _db_ctx.transactions = _db_ctx.transactions + 1
+        return self
+
+    def __exit__(self, exctype, excvalue, traceback):
+        global _db_ctx
+        _db_ctx.transactions = _db_ctx.transactions - 1
+        try:
+            if _db_ctx.transactions==0:
+                if exctype is None:
+                    self.commit()
+                else:
+                    self.rollback()
+        finally:
+            if self.should_close_conn:
+                _db_ctx.cleanup()
+
+    def commit(self):
+        global _db_ctx
+        try:
+            _db_ctx.connection.commit()
+        except:
+            _db_ctx.connection.rollback()
+            raise
+
+    def rollback(self):
+        global _db_ctx
+        _db_ctx.connection.rollback()
