1f7327bdf29f60b6b5f9e671fe93994429f29d55
southjw <southjw@163.com>
2015-09-09 09:41:20 +0800

    add file

--- coroutine.py
+++ coroutine.py
@@ -1,22 +1,20 @@
 import asyncio
 
 @asyncio.coroutine
-def wget(host):
-    print('wget %s...' % host)
-    connect = asyncio.open_connection(host, 80)
-    reader, writer = yield from connect
-    header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
-    writer.write(header.encode('utf-8'))
-    yield from writer.drain()
-    while True:
-        line = yield from reader.readline()
-        if line == b'\r\n':
-            break
-        print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
-    # Ignore the body, close the socket
-    writer.close()
+def test():
+    print("test start!...")
+    r = yield from asyncio.sleep(1)
+    print("test end!...")
 
+@asyncio.coroutine
+def hello():
+    print("Hello world!")
+    # 异步调用asyncio.sleep(1):
+    r = yield from test()
+    print("Hello again!")
+
+# 获取EventLoop:
 loop = asyncio.get_event_loop()
-tasks = [wget(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
-loop.run_until_complete(asyncio.wait(tasks))
+# 执行coroutine
+loop.run_until_complete(hello())
 loop.close()
