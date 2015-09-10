99fd5ca663a27a6ea483d421745751477a98eb36
southjw <southjw@163.com>
2015-09-10 16:00:54 +0800

    add file

--- hello.py
+++ hello.py
@@ -1,3 +1,24 @@
+#import os
+#def getEncodeStr(word,val='utf-8'):
+#    if os.name=='nt':
+#        wtemp=u'wtemp'
+#        if type(word)==type(wtemp):
+#            disvalue=word
+#        else:
+#            disvalue=unicode(word, 'mbcs')
+#        if val.lower()!='utf-8':
+#            try:
+#                tdv=disvalue.encode(val)
+#            except:
+#                tdv=disvalue.encode('utf-8')
+#        else:
+#            tdv=disvalue.encode(val)
+#        return tdv
+#    else:
+#        return word
+
+
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return '<h1>Hello, web!</h1>'
+    body = ('<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or 'web'))
+    return [body.encode('utf-8')]
